ae7388ce0b0233cbadde04a85c502fd67ab15219
Anton Hesse <ahesse2567@gmail.com>
2021-12-14 16:16:56 -0600

    spline didn't come from the splines package, but rather from stats

--- R/interpolate.R
+++ R/interpolate.R
@@ -16,7 +16,7 @@ interpolate <- function(.data, time_col, every_s = 1, method = c("linear", "cubi
     # do we need to coerce everything to numeric and save the character cols?
 
     if(method == "cubic") {
-        out <- purrr::map(.x = data_num, .f = function(i) splines::spline(
+        out <- purrr::map(.x = data_num, .f = function(i) stats::spline(
             x = data_num[[time_col]],
             y = i,
             xout = per_every)$y) %>%
