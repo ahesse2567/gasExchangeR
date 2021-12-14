b8f0ebd8ce735e4516f94c2312a86f06f7f64b75
Anton Hesse <ahesse2567@gmail.com>
2021-12-14 14:27:34 -0600

    Made start_end_test more flexible by adding additional arguments

--- R/start_end_test.R
+++ R/start_end_test.R
@@ -1,6 +1,9 @@
-start_end_test <- function(.data, start_speed) {
-    start <- which(lag(diff(.data$speed)) == 3)
-    end <- which(diff(.data$speed) < 0)
+start_end_test <- function(.data,
+                           intensity_col,
+                           start_speed,
+                           end_speed = 0) {
+    start <- which(lag(diff(.data[[intensity_col]])) == start_speed)
+    end <- which(diff(.data[[intensity_col]]) < end_speed)
     if(length(end) == 0) { # test was terminated before clicking recovery on the computer
         end <- nrow(.data) # instead, set end to last data point in file
     }
