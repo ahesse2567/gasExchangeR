f4cecfb80b9e409b9ca814f0361588d12188cefe
Anton Hesse <ahesse2567@gmail.com>
2021-12-14 16:11:53 -0600

    Interpolation function added. It will later need error handling as well as linear and cubic methods. However, it works!

---
+++ R/interpolate.R
@@ -0,0 +1,33 @@
+interpolate <- function(.data, time_col, every_s = 1, method = c("linear", "cubic")) {
+    # TODO add stopifnot()
+    method <- match.arg(method, choices = c("linear", "cubic"))
+    # turn this into a method later
+    # class(.data) <- method
+    # UseMethod("interpolate", .data)
+
+    data_num <- .data %>% # coerce to numeric b/c time may not be of another class
+        dplyr::mutate(dplyr::across(where(purrr::negate(is.character)),
+                                    as.numeric))
+    # add removal of NA vals to the numeric coercion above?
+
+    per_every <- seq(from = min(as.integer(df[[time_col]])),
+                     to = as.integer(max(df[[time_col]])), by = every_s)
+
+    # do we need to coerce everything to numeric and save the character cols?
+
+    if(method == "cubic") {
+        out <- purrr::map(.x = data_num, .f = function(i) spline(
+            x = data_num[[time_col]],
+            y = i,
+            xout = per_every)$y) %>%
+            dplyr::as_tibble()
+    } else {
+        out <- purrr::map(.x = data_num, .f = function(i) approx(
+            x = data_num[[time_col]],
+            y = i,
+            xout = per_every)$y) %>%
+            dplyr::as_tibble()
+    }
+
+    out
+}
