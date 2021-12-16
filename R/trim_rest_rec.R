be06134422fe322c6005c8de1c4b0fab9fdb6940
Anton Hesse <ahesse2567@gmail.com>
2021-12-16 08:26:15 -0600

    Changed the name of start_end_test to trim_rest_rec because I think the name is a little bit more descriptive

--- R/trim_rest_rec.R
+++ R/trim_rest_rec.R
@@ -1,9 +1,9 @@
-#' Delete any 'pre-exercise' and 'recovery' portions from a graded exercise test
+#' Delete any 'resting/pre-exercise' and 'recovery' portions from a graded exercise test
 #'
-#' @param .data The data frame of the graded exercise test
+#' @param .data Unaveraged, breath-by-breath exercise test
 #' @param intensity_col Speed or wattage values that help determine start and end of a test
 #' @param start_intensity Initial intensity just after 'pre-exercise' data
-#' @param end_intensity Intensity that denotes the test has ended
+#' @param end_intensity Intensity that denotes the test has ended or entered the recovery phase
 #'
 #' @return
 #' @export
@@ -11,8 +11,8 @@
 #' @examples
 #' df <- data.frame(time = c(14, 18, 61, 78, 88, 100, 120, 150, 220, 231),
 #' speed = c(0, 0, 0, 0, 3, 3, 3, 7.4, 7.4, 0))
-#' start_end_test(.data = df, intensity_col = "speed", start_intensity = 3)
-start_end_test <- function(.data,
+#' trim_rest_rec(.data = df, intensity_col = "speed", start_intensity = 3)
+trim_rest_rec <- function(.data,
                            intensity_col,
                            start_intensity,
                            end_intensity = 0) {
