cfd30dc0163a4c70afc606bc46d23018a69f5657
Anton Hesse <ahesse2567@gmail.com>
2022-01-04 14:20:32 -0600

    The best b-spline models have TONS of knots (`50+). The paper "The Respiratory Compensation Point is Not a Valid Surrogate for Critical Power" used 16 knots because it did "not overfit the data". That paper didn't describe how they determined that the data was not overfiteed.

--- vent_eqs_breakpoint_testing.R
+++ vent_eqs_breakpoint_testing.R
@@ -4,29 +4,23 @@ library(splines)
 library(AICcmodavg)
 library(ggfortify)
 library(car)
+library(broom)
 
-df <- read_csv("inst/extdata/37818_vo2max_unavg.csv")
-df <- df %>%
+df_raw <- read_csv("inst/extdata/37818_vo2max_unavg.csv")
+df_unavg <- df_raw %>%
     rename_with(.fn = tolower) %>%
     rename(vo2_rel = vo2...4,
            vo2_abs = vo2...5,
            ve = `ve btps`,
            vt = `vt btps`) %>%
     select(time, speed, grade, vo2_rel, vo2_abs, vco2, ve) %>%
-    filter(speed > 3.5) %>% # after beginning steady state portion
-    slice(1:which.max(ve))
-
-df <- df %>%
+    trim_rest_rec(intensity_col = "grade",
+                  pre_ex_intensity = 300.1,
+                  end_ex_intensity = 300.1) %>%
     mutate(ve_vo2 = ve / (vo2_abs/1000),
            ve_vco2 = ve / (vco2/1000))
 
-df_avg <- avg_exercise_test(df,
-                            type = "breath",
-                            subtype = "rolling",
-                            roll_window = 9,
-                            trim = 2)
-
-ggplot(data = df_avg, aes(x = time)) +
+ggplot(data = df_unavg, aes(x = time)) +
     geom_point(aes(y = ve_vo2), color = "orange") +
     geom_point(aes(y = ve_vco2), color = "green") +
     theme_bw()
@@ -35,47 +29,63 @@ knot_max <- 100
 bs_mod_list <- vector(mode = "list", length = length(seq_along(3:knot_max)))
 for (i in 3:knot_max) {
     spline_mod_name <- paste0("bs.", i)
-    spline_mod <- lm(ve_vo2 ~ 1 + bs(time, df = i + 3), data = df_avg)
-    assign(spline_mod_name, spline_mod)
-    bs_mod_list[[i - 2]] <- spline_mod
+    bs_mod_list[[i-2]] <- lm(ve_vo2 ~ 1 + bs(time,
+                                             df = i + 3,
+                                             degree = 3),
+                             data = df_unavg)
+    names(bs_mod_list)[i-2] <- spline_mod_name
 }
 
-bs_mod_names <- character(length = length(3:knot_max))
-for (i in 3:knot_max) {
-    bs_mod_names[i - 2] <- paste("BS, ", i, " knots (df = ", i + 3, ")",
-                                 sep = "")
-}
-
-bs_mod_comp <- aictab(cand.set = bs_mod_list, modnames = bs_mod_names) %>%
+bs_mod_comp <- aictab(cand.set = bs_mod_list, modnames = names(bs_mod_list)) %>%
     as_tibble() %>%
     dplyr::select(1:4)
 
-best_bs <- bs_mod_comp <- aictab(cand.set = bs_mod_list,
-                                 modnames = bs_mod_names) %>%
-    as_tibble() %>%
-    dplyr::filter(AICc == min(AICc)) %>%
-    dplyr::select(1:3)
-best_bs
-autoplot(bs.42, which = 1:6, ncol = 3, label.size = 3)
-influenceIndexPlot(bs.42)
-outlierTest(bs.42)
+best_bs <- bs_mod_comp %>%
+    dplyr::filter(AICc == min(AICc))
 
-ggplot(data = df_avg, aes(x = time, y = ve_vo2)) +
-    geom_point(color = "orange") +
-    geom_smooth(method = "lm", formula = y ~ bs(x, 45)) +
-    theme_bw()
+best_bs_idx <- which(names(bs_mod_list) == best_bs$Modnames)
+
+autoplot(bs_mod_list[best_bs_idx], which = 1:6, ncol = 3, label.size = 3)
+influenceIndexPlot(bs_mod_list[[best_bs_idx]])
+outlierTest(bs_mod_list[[best_bs_idx]])
 
 plot_data = tibble(
     time = seq(from = min(df_avg$time),
                  to = max(df_avg$time),
-                 by = 1)) %>%
-    mutate(
-        yhat = predict(lm_ve_vo2.6, newdata = .),
-    )
+                 by = 1))
+
+pred <- predict(bs_mod_list[[best_bs_idx]],
+                interval = "prediction") %>%
+    as_tibble()
+bs_mod_list[[best_bs_idx]]$fitted.values
+
+pred <- pred %>%
+    mutate(outlier = if_else(fit < lwr | fit > upr, TRUE, FALSE))
+
+pred$fit > pred$upr
+
+plot_data <- df_unavg %>%
+    select(time, ve_vo2) %>%
+    bind_cols(pred)
+
+ggplot(data = plot_data, aes(x = time, y = ve_vo2)) +
+    geom_point(alpha = 0.5) +
+    geom_line(aes(y = fit)) +
+    geom_line(aes(y = lwr), linetype = "dashed") +
+    geom_line(aes(y = upr), linetype = "dashed") +
+    theme_bw()
+
+ggplot(data = plot_data, aes(x = time, y = yhat)) +
+    geom_line(color = "black") +
+    geom_line(aes(y = .pred_lwr), linetype = "dashed", color = "purple") +
+    geom_line(aes(y = .pred_upr), linetype = "dashed", color = "purple") +
+    geom_point(data = df_unavg, aes(x = time, y = ve_vo2),
+               color = "green", alpha = 0.5) +
+    theme_bw()
 
 ggplot(data = plot_data, aes(x = time, y = yhat)) +
     geom_line() +
-    geom_point(data = df_avg, aes(x = time, y = ve_vo2), color = "orange") +
+    geom_point(data = df_unavg, aes(x = time, y = ve_vo2), color = "orange") +
     theme_bw()
 
 tidy(lm_ve_vo2)
