f84fc69950169e7f47989b2e6e1781a949b6ae7e
Anton Hesse <ahesse2567@gmail.com>
2022-01-14 10:13:06 -0600

    Adding placeholder script for breakpoint functions. Tried new breakpoint methods within func_testing.R

--- func_testing.R
+++ func_testing.R
@@ -4,12 +4,13 @@ library(tidyverse)
 df_raw <- read_csv("inst/extdata/mar16_101_pre.csv")
 
 df_unavg <- df_raw %>%
-    trim_rest_rec(intensity_col = "speed") %>%
+    rename_all(.funs = tolower) %>%
     rename(vo2_rel = vo2,
            vo2_abs = vo2.1,
            ve = ve.btps) %>%
-    # vo2max_window() %>%
-    # x_breath_mean(b = 4) %>%
+    trim_rest_rec(intensity_col = "speed") %>%
+    trim_rest_rec(intensity_col = "speed", pre_ex_intensity = 3) %>%
+    trim_rest_rec(intensity_col = "speed", pre_ex_intensity = 5.6) %>%
     select(time, speed, grade, vo2_rel, vo2_abs, vco2, ve, peto2, petco2) %>%
     mutate(ve_vo2 = ve*1000 / vo2_abs,
            ve_vco2 = ve*1000 / vco2)
@@ -17,98 +18,38 @@ df_unavg <- df_raw %>%
 df_avg <- df_unavg %>%
     avg_exercise_test(type = "breath",
                       subtype = "rolling",
-                      roll_window = 15)
+                      roll_window = 11)
 
-ggplot(data = df_avg, aes(x = time, y = ve)) +
-    geom_point(color = "orange", alpha = 0.5) +
-    theme_bw()
+ggplot(data = df_avg, aes(x = time, y = petco2)) +
+    geom_point(color = "red", alpha = 0.5) +
+    theme_bw() +
+    ylim(c(30, 45))
 
-df_inc <- df_avg %>%
-    filter(speed > 5.6)
+ggplot(data = df_avg, aes(x = vco2, y = ve)) +
+    geom_point(color = "brown", alpha = 0.5) +
+    theme_bw() +
+    geom_smooth(method = "lm")
 
-ggplot(data = df_inc, aes(x = time)) +
-    geom_point(aes(y = vo2_abs/1000), color = "red", alpha = 0.5) +
-    geom_point(aes(y = vco2/1000), color = "blue", alpha = 0.5) +
-    # geom_point(aes(y = ve), color = "orange", alpha = 0.5) +
-    theme_bw()
-
-ggplot(data = df_inc, aes(x = time)) +
-    geom_point(aes(y = petco2), color = "blue", alpha = 0.5) +
-    geom_point(aes(y = ve_vco2), color = "green", alpha = 0.5) +
-    ylim(c(20, 45)) +
-    theme_bw()
-
-## Nile data with one breakpoint: the annual flows drop in 1898
-## because the first Ashwan dam was built
-data("Nile")
-plot(Nile)
-
-## F statistics indicate one breakpoint
-fs.nile <- Fstats(Nile ~ 1)
-plot(fs.nile)
-breakpoints(fs.nile)
-lines(breakpoints(fs.nile))
-
-## or
-bp.nile <- breakpoints(Nile ~ 1)
-summary(bp.nile)
-
-## the BIC also chooses one breakpoint
-plot(bp.nile)
-breakpoints(bp.nile)
-
-## fit null hypothesis model and model with 1 breakpoint
-fm0 <- lm(Nile ~ 1)
-fm1 <- lm(Nile ~ breakfactor(bp.nile, breaks = 1))
-plot(Nile)
-lines(ts(fitted(fm0), start = 1871), col = 3)
-lines(ts(fitted(fm1), start = 1871), col = 4)
-lines(bp.nile)
-
-## confidence interval
-ci.nile <- confint(bp.nile)
-ci.nile
-lines(ci.nile)
-
-### strucchange with exercise data
-plot(df_inc$ve_vco2)
-fs.ve_vco2 <- Fstats(df_inc$ve_vco2 ~ 1)
-plot(fs.ve_vco2)
-breakpoints(fs.ve_vco2)
-lines(breakpoints(fs.ve_vco2))
+lm1 <- lm(time ~ petco2 + 1, data = df_avg)
+lm1
+out <- segmented::segmented(lm1)
+out
+plot(out)
+points(x = df_avg$vco2, y = df_avg$ve_vco2)
 
 
-bp.ve_vco2 <- breakpoints(df_inc$ve_vco2 ~ 1)
-summary(bp.ve_vco2)
-plot(bp.ve_vco2)
-breakpoints(bp.ve_vco2)
+ffset.seed(12)
+xx<-1:100
+zz<-runif(100)
+yy<-2+1.5*pmax(xx-35,0)-1.5*pmax(xx-70,0)+15*pmax(zz-.5,0)+rnorm(100,0,2)
+dati<-data.frame(x=xx,y=yy,z=zz)
+out.lm<-lm(y~x,data=dati)
 
-fm0 <- lm(df_inc$ve_vco2 ~ 1)
-bp.ve_vco2 <- breakpoints(df_inc$ve_vco2 ~ 1)
-fm1 <- lm(df_inc$ve_vco2 ~ breakfactor(bp.ve_vco2, breaks = 4))
-plot(df_inc$ve_vco2)
-lines(ts(fitted(fm0)), col = 3)
-lines(ts(fitted(fm1)), col = 4)
-lines(bp.ve_vco2)
+ggplot(data = dati, aes(x = x, y = y)) +
+    geom_point()
 
-
-### with petco2
-plot(df_inc$petco2)
-fs.petco2 <- Fstats(df_inc$petco2 ~ 1)
-plot(fs.petco2)
-breakpoints(fs.petco2)
-lines(breakpoints(fs.petco2))
-
-
-bp.petco2 <- breakpoints(df_inc$petco2 ~ 1)
-summary(bp.petco2)
-plot(bp.petco2)
-breakpoints(bp.petco2)
-
-fm0 <- lm(df_inc$petco2 ~ 1)
-bp.petco2 <- breakpoints(df_inc$petco2 ~ 1)
-fm1 <- lm(df_inc$petco2 ~ breakfactor(bp.petco2, breaks = 2))
-plot(df_inc$petco2)
-lines(ts(fitted(fm0)), col = 3)
-lines(ts(fitted(fm1)), col = 4)
-lines(bp.ve_vco2)
+#the simplest example: the starting model includes just 1 covariate
+#.. and 1 breakpoint has to be estimated for that
+o<-segmented(out.lm) #1 breakpoint for x
+class(o)
+plot(o)
