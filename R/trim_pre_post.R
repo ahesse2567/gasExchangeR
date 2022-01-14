f4914b2b4108e11f5244c11b2815c526fed7809c
Anton Hesse <ahesse2567@gmail.com>
2022-01-14 09:37:43 -0600

    Renaming trim_rest_rec to trim_pre_post b/c I kept wanting to write trim_pre_post

--- R/trim_pre_post.R
+++ R/trim_pre_post.R
@@ -1,9 +1,9 @@
-#' Delete any 'resting/pre-exercise' and 'recovery' portions from a graded exercise test
+#' Delete any 'resting/pre-exercise' and 'recovery/post-exercise' portions from a graded exercise test
 #'
 #' @param .data Unaveraged, breath-by-breath exercise test.
 #' @param intensity_col Speed, wattage, or grade values that help determine start and end of a test.
 #' @param pre_ex_intensity Intensity of 'pre-exercise' data.
-#' @param end_ex_intensity Intensity that denotes the test has ended or entered the final recovery phase.
+#' @param post_ex_intensity Intensity that denotes the test has ended or entered the final recovery phase.
 #'
 #' @return
 #' @export
@@ -11,17 +11,18 @@
 #' @examples
 #' df <- data.frame(time = c(14, 18, 61, 78, 88, 100, 120, 150, 220, 231),
 #' speed = c(0, 0, 0, 0, 3, 3, 3, 7.4, 7.4, 0))
-#' trim_rest_rec(.data = df, intensity_col = "speed")
-trim_rest_rec <- function(.data,
+#' trim_pre_post(.data = df, intensity_col = "speed")
+trim_pre_post <- function(.data,
                           intensity_col,
                           pre_ex_intensity = 0,
-                          end_ex_intensity = 0) {
+                          post_ex_intensity = 0) {
+    # browser()
     rle_start_intensity <- rle(.data[[intensity_col]] == pre_ex_intensity)
     start_idx <- rle_start_intensity$lengths[1] + 1 # index 1 gets first change,
     # but needs to add 1 to account for how rle gives you the index
     # of each run.
 
-    rle_end_intensity <- rle(rev(.data[[intensity_col]] == end_ex_intensity))
+    rle_end_intensity <- rle(rev(.data[[intensity_col]] == post_ex_intensity))
     from_end_idx <- rle_end_intensity$lengths[1]
     end_idx <- nrow(.data) - from_end_idx
     if(end_idx < start_idx) {
