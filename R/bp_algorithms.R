2ff1a32288c11590f05802e6e92c2941f085fb2b
Anton Hesse <ahesse2567@gmail.com>
2022-02-15 15:36:59 -0600

    breakpoints now has params for vt1 and vt2
    I'm getting a useMethod error. I think I need to use switch statements though b/c I'm going to have so many different functions. It will help to have all the algorithms be in their own script.

---
+++ R/bp_algorithms.R
@@ -0,0 +1,143 @@
+#' Breakpoint Algorithm
+#'
+#' @param .data Gas exchange data.
+#' @param algorithm The specific breakpoint algorithm you want to use to find a given threshold. Choices include \code{dmax}, \code{dmax_mod}, \code{jm}, \code{orr}, \code{v-slope}, \code{v_slope_simple}, and \code{splines}.
+#' @param .x The x-axis variable
+#' @param .y the y-axis variable
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # Come up with an example later
+#'
+bp_algorithm <- function(.data,
+                         algorithm,
+                         .x,
+                         .y,
+                         vo2 = "vo2",
+                         vco2 = "vco2",
+                         ve = "ve",
+                         time = "time") {
+    stopifnot(!missing(.data), !missing(.x), !missing(.y), !missing(algorithm))
+    # browser()
+
+    algorithm <- match.arg(algorithm,
+                           choices = c("dmax", "dmax_mod", "jm",
+                                       "orr", "v-slope", "v_slope_simple",
+                                       "splines")) # several ok = FALSE?
+
+    class(.data) <- append(class(.data), algorithm) # could be useful to
+    UseMethod("bp_algorithm", .data)
+}
+
+bp_algorithm.jm <- function(.data,
+                            algorithm,
+                            .x,
+                            .y,
+                            vo2 = "vo2",
+                            vco2 = "vco2",
+                            ve = "ve",
+                            time = "time") {
+    min_ss_idx <- which.min(loop(.data,
+                          algorithm,
+                          .x,
+                          .y,
+                          vo2 = "vo2",
+                          vco2 = "vco2",
+                          ve = "ve",
+                          time = "time"))
+
+    df_left <- .data[1:min_ss_idx,] # x < x0
+    df_right <- .data[(min_ss_idx+1):nrow(.data),] # x >= x0
+
+    df_right <- df_right %>%
+        mutate(s1 = df_right[[.x]] - .data[[.x]][min_ss_idx+1])
+
+    lm_left <- lm(df_left[[.y]] ~ 1 + df_left[[.x]], data = df_left)
+    # according to the JM method, the right regression line will have a constant equal to b0 + b1*x0
+    b0_plus_b1x0 <- lm_left$coefficients[1] + lm_left$coefficients[2] * .data[[.x]][min_ss_idx+1]
+
+    # for some reason, using I() function to try to force the regression to use b0_plus_b1x0 as an intercept doesn't work, while the offset argument does. The slope coefficient (b3) is the same, but the predicted values are not.
+    lm_right <- lm(df_right[[.y]] ~ 0 + s1, data = df_right,
+                   offset = rep(b0_plus_b1x0, nrow(df_right)))
+
+    y_hat_left <- tibble(x = df_left[[.x]],
+                         y_hat = lm_left$fitted.values,
+                         method = "jm")
+    y_hat_right <- tibble(x = df_right[[.x]],
+                          y_hat = lm_right$fitted.values,
+                          method = "jm")
+    pred <- bind_rows(y_hat_left, y_hat_right)
+    pct_slope_change <- 100*(lm_right$coefficients[1] - lm_left$coefficients[2]) /
+        lm_left$coefficients[2]
+
+    vo2_col <- stringr::str_which(colnames(df), vo2)
+    jm_row <- .data[min_ss_idx+1,] %>%
+        select(time, vo2_col, .x, .y, method)
+
+    return(list(jm_row, pred, pct_slope_change))
+
+}
+
+#' @keywords internal
+loop <- function(.data,
+                 algorithm,
+                 .x,
+                 .y,
+                 vo2 = "vo2",
+                 vco2 = "vco2",
+                 ve = "ve",
+                 time = "time") {
+    loop <- match.arg(algorithm, choices = c("jm", "orr", "v-slope",
+                                         "v_slope_simple", "splines"))
+    class(.data) <- append(class(.data), loop)
+    UseMethod("loop", .data)
+}
+
+#' @keywords internal
+loop.jm <- function(.data,
+                    algorithm,
+                    .x,
+                    .y,
+                    vo2 = "vo2",
+                    vco2 = "vco2",
+                    ve = "ve",
+                    time = "time") {
+    # browser()
+    .data <- .data %>%
+        arrange(.data[[.x]], .data[[time]])
+
+    ss_both <- numeric(length = nrow(.data)-2)
+    for(i in 2:(nrow(.data)-1)) {
+        # algorithm data into left and right halves. x0 = i+1
+        .data_left <- .data[1:i,] # x < x0
+        .data_right <- .data[(i+1):nrow(.data),] # x >= x0
+
+        # the JM formula is y = b0 + b1*x0 + b3(x-x0). Therefore, we will create a new column in .data_right that is equal to x - x0.
+        .data_right <- .data_right %>%
+            mutate(s1 = .data_right[[.x]] - .data[[.x]][i+1])
+
+        .data_right$s1 = .data_right[[.x]] - .data[[.x]][i+1]
+
+        lm_left <- lm(.data_left[[.y]] ~ 1 + .data_left[[.x]], data = .data_left)
+
+        # according to the JM method, the right regression line will have a constant equal to b0 + b1*x0
+        b0_plus_b1x0 <- lm_left$coefficients[1] + lm_left$coefficients[2] * .data[[.x]][i+1]
+
+        # for some reason, using I() function to try to force the regression to use b0_plus_b1x0 as an intercept doesn't work, while the offset argument does. The slope coefficient (b3) is the same, but the predicted values are not.
+        lm_right <- lm(.data_right[[.y]] ~ 0 + s1, data = .data_right,
+                       offset = rep(b0_plus_b1x0, nrow(.data_right)))
+
+        # This should be the same as using the offset argument, but it's not
+        # lm_right <- lm(I(.data[[.y]] - b0_plus_b1x0) ~ 0 + .data[[.x]], data = .data_right)
+
+        ss_left <- sum((lm_left$residuals)^2)
+        ss_right <- sum((lm_right$residuals)^2)
+        ss_both[i-1] <- (ss_left + ss_right)
+
+        #ss_both <- c(ss_both, ss_left + ss_right)
+    }
+
+    ss_both
+}
--- func_testing.R
+++ func_testing.R
@@ -1,7 +1,7 @@
 library(gasExchangeR)
 library(tidyverse)
 
-df_raw <- read_csv("inst/extdata/mar16_101_pre.csv")
+df_raw <- read_csv("inst/extdata/mar16_101_pre.csv", show_col_types = FALSE)
 
 df_unavg <- df_raw %>%
     rename_all(.funs = tolower) %>%
@@ -19,38 +19,12 @@ ggplot(data = df_unavg, aes(x = time, y = vo2_abs)) +
     geom_point(color = "red", alpha = 0.5) +
     theme_bw()
 
-df_avg <- avg_exercise_test(df_unavg, type = "breath", subtype = "bin",
-                  time_col = "time", bin_w = 10, trim = 2)
+df_avg <- avg_exercise_test(df_unavg, type = "breath", subtype = "rolling",
+                  time_col = "time", roll_window = 9, trim = 4)
 
 ggplot(data = df_avg, aes(x = time, y = vo2_abs)) +
     geom_point(color = "red", alpha = 0.5) +
     theme_bw()
 
-ggplot(data = df_avg, aes(x = vco2, y = ve)) +
-    geom_point(color = "brown", alpha = 0.5) +
-    theme_bw() +
-    geom_smooth(method = "lm")
-
-lm1 <- lm(time ~ petco2 + 1, data = df_avg)
-lm1
-out <- segmented::segmented(lm1)
-out
-plot(out)
-points(x = df_avg$vco2, y = df_avg$ve_vco2)
-
-
-ffset.seed(12)
-xx<-1:100
-zz<-runif(100)
-yy<-2+1.5*pmax(xx-35,0)-1.5*pmax(xx-70,0)+15*pmax(zz-.5,0)+rnorm(100,0,2)
-dati<-data.frame(x=xx,y=yy,z=zz)
-out.lm<-lm(y~x,data=dati)
-
-ggplot(data = dati, aes(x = x, y = y)) +
-    geom_point()
-
-#the simplest example: the starting model includes just 1 covariate
-#.. and 1 breakpoint has to be estimated for that
-o<-segmented(out.lm) #1 breakpoint for x
-class(o)
-plot(o)
+breakpoint(df_avg, algorithm_aert = "jm", x_aert = "ve_vo2", y_aert = "time",
+           algorithm_rc = "jm", x_rc = "ve_vco2", y_rc = "time", vo2 = "vo2_abs")
