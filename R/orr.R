b7654a99bd9bf3c70051649d7dc34c5aceec590f
Anton Hesse <ahesse2567@gmail.com>
2022-02-26 16:22:02 -0600

    Orr code currently returns the index of the breakpoint. The next step is to return the values associated with that breakpoint.

--- R/orr.R
+++ R/orr.R
@@ -16,6 +16,7 @@
 #' Orr, G. W., Green, H. J., Hughson, R. L., & Bennett, G. W. (1982). A computer linear regression model to determine ventilatory anaerobic threshold. Journal of Applied Physiology Respiratory Environmental and Exercise Physiology, 52(5), 1349–1352. https://doi.org/10.1152/jappl.1982.52.5.1349
 #'
 #' @examples
+#' # TODO write examples
 orr <- function(.data,
                 algorithm,
                 .x,
@@ -28,40 +29,20 @@ orr <- function(.data,
     ss <- loop_orr(.data = .data, .x = .x, .y = .y)
 
     min_ss_idx <- which.min(ss)
-    min_ss_
+    min_ss_idx
+
+    # now we need to find the closest data point to the breakpoint
 }
-
-orr(.data = df_avg, .x = "vo2_abs", .y = "vco2", vo2 = "vo2_abs")
-
+#'
+#' @keywords internal
 loop_orr <- function(.data, .x, .y) {
-    browser()
+    # browser()
     ss_both <- vector(length = nrow(.data)-2)
     for(i in 2:(nrow(.data)-1)) {
         # split data into left and right halves
         # should these share the same point? Or should, they be different by 1 point?
         df_left <- .data[1:i,]
         df_right <- .data[(i):nrow(.data),] # each side of the regression is contiguous
-
-        # make linear models of the two halves
-        lm_left <- lm(df_left[[.y]] ~ 1 + df_left[[.x]], data = df_left)
-        lm_right <- lm(df_right[[.y]] ~ 1 + df_right[[.x]], data = df_right)
-
-        ss_left <- sum((lm_left$residuals)^2)
-        ss_right <- sum((lm_right$residuals)^2)
-        #ss_both <- c(ss_both, ss_left + ss_right)
-        ss_both[i-1] <- (ss_left + ss_right)
-    }
-    return(ss_both)
-}
-
-
-loop.orr <- function(df, .x, .y){
-    ss_both <- vector(length = nrow(df)-3)
-    for(i in 2:(nrow(df)-2)) {
-        # split data into left and right halves
-        # should these share the same point? Or should, they be different by 1 point?
-        df_left <- .data[1:i,]
-        df_right <- .data[(i+1):nrow(df),] # currently set up to differ by 1 point
 
         # make linear models of the two halves
         lm_left <- lm(df_left[[.y]] ~ 1 + df_left[[.x]], data = df_left)
