cc63d3adedf4b5efd9db68474f583cb4a6ca54c2
Anton Hesse <ahesse2567@gmail.com>
2022-03-02 08:44:42 -0600

    V-slope code currently finds breakpoint index. Next it needs to calculate the actual breakpoint value.

--- R/v-slope.R
+++ R/v-slope.R
@@ -15,6 +15,7 @@
 #' Beaver, W. L., Wasserman, K., & Whipp, B. J. (1986). A new method for detecting anaerobic threshold by gas exchange. Journal of Applied Physiology, 121(6), 2020–2027.
 #'
 #' @examples
+#' # TODO write an example
 v_slope <- function(.data,
                     .x,
                     .y,
@@ -22,14 +23,35 @@ v_slope <- function(.data,
                     vco2 = "vco2",
                     ve = "ve",
                     time = "time") {
-    browser()
-    lm_simple <- lm(.data[[.y]] ~ 1 + .data[[.x]])
+    # browser()
+    # TODO exclude data at the beginning if the VCO2 vs. VO2 slopeis < 0.6
+    ss <- loop_v_slope(.data = .data, .x = .x, .y = .y)
+    slope_change <- 0
+    i <- 1
+    while(slope_change < 0.1) {
+        bp_idx <- order(-ss)[i]
+        df_left <- df_avg[1:bp_idx,]
+        df_right <- df_avg[(bp_idx+1):nrow(df_avg),]
+        lm_left <- lm(vco2 ~ 1 + vo2_abs, data = df_left)
+        lm_right <- lm(vco2 ~ 1 + vo2_abs, data = df_right)
 
+        slope_change <- lm_right$coefficients[2] - lm_left$coefficients[2]
+        i <- i + 1
+        if (i > length(ss)) {
+            stop("No breakpoint found. Change between slopes was never >= 0.1.")
+        }
+    }
 
+    bp_idx
+    # TODO find crossing point of left and right regression
 }
 
+#' @keywords internal
 loop_v_slope <- function(.data, .x, .y) {
+    # browser()
     lm_simple <- lm(.data[[.y]] ~ 1 + .data[[.x]], data = .data)
+    # find slope of line perpendicular to slope of lm_simple
+    recip_slope <- (-1 / lm_simple$coefficients[2]) # used in for loop
     dist_MSE_ratio <- vector(length = nrow(.data)-3)
     for(i in 2:(nrow(.data)-2)) { # nrow(df)-2) b/c these DON'T share a point
         # the curve is divided into two regions, each of which is fitted by linear regression
@@ -47,8 +69,6 @@ loop_v_slope <- function(.data, .x, .y) {
             (lm_left$coefficients[2] - lm_right$coefficients[2])
         y_left_right <- lm_left$coefficients[1] + lm_left$coefficients[2]*x_left_right
 
-        # find slope of line perpandicular to slope of lm_simple
-        recip_slope <- (-1 / lm_simple$coefficients[2])
         b_recip <- recip_slope * (-1) * x_left_right + y_left_right
 
         x_simple_recip <- (b_recip - lm_simple$coefficients[1]) /
@@ -59,42 +79,8 @@ loop_v_slope <- function(.data, .x, .y) {
         d <- sqrt((x_simple_recip - x_left_right)^2 +
                       (y_simple_recip - y_left_right)^2)
 
-        #######
-        # x = (b2 - b1) / (m1 - m2)
-        # treat b2 and m2 as from the simple regression line
-        x_left_right <- (lm_right$coefficients[1] - lm_left$coefficients[1]) /
-            (lm_left$coefficients[2] - lm_right$coefficients[2])
-        y_left_right <- lm_left$coefficients[1] + lm_left$coefficients[2]*x_left_right
-
-        x_left_simple <- (lm_simple$coefficients[1] - lm_left$coefficients[1]) /
-            (lm_left$coefficients[2] - lm_simple$coefficients[2])
-        y_left_simple <- lm_left$coefficients[1] + lm_left$coefficients[2]*x_left_simple
-
-        x_right_simple <- (lm_simple$coefficients[1] - lm_right$coefficients[1]) /
-            (lm_right$coefficients[2] - lm_simple$coefficients[2])
-        y_right_simple <- lm_left$coefficients[1] + lm_left$coefficients[2]*x_right_simple
-
-        dist_left_simple <- sqrt((x_left_simple - x_left_right)^2 +
-                                     (y_left_simple - y_left_right)^2)
-
-        dist_right_simple <- sqrt((x_right_simple - x_left_right)^2 +
-                                      (y_right_simple - y_left_right)^2)
-
-        if(dist_left_simple >= dist_right_simple) {
-            dist <- dist_left_simple
-        } else {
-            dist <- dist_right_simple
-        }
-
-        # we need to calculate the mean square error of the regression, but the Beaver paper doesn't indicate which regression.
-        #Is it the MSE of the single regression line? Or is it the MSE of the left and right regression lines?
-        #Let's assume for now that it's the single regression line b/c that's easier
-
-        deg_free <- nrow(df) - 2 # we're subtracting 2 b/c we're estimating 2 parameters: the intercept and the slope
-        MSE_simple <- sum(lm_simple$residuals^2) / deg_free
-        dist_MSE_ratio[i-1] <- (dist / MSE_simple)
-        #dist_MSE_ratio <- c(dist_MSE_ratio, dist / MSE_simple)
+        dist_MSE_ratio[i-1] <- d / anova(lm_simple)['Residuals', 'Mean Sq']
     }
 
-    return(dist_MSE_ratio)
+    dist_MSE_ratio
 }
---
+++ code_testing/v_slope_testing.R
@@ -0,0 +1,42 @@
+library(gasExchangeR)
+library(tidyverse)
+library(devtools)
+
+df_raw <- read_csv("inst/extdata/mar16_101_pre.csv", show_col_types = FALSE)
+
+df_unavg <- df_raw %>%
+    rename_all(.funs = tolower) %>%
+    rename(vo2_rel = vo2,
+           vo2_abs = vo2.1,
+           ve = ve.btps) %>%
+    trim_pre_post(intensity_col = "speed") %>%
+    trim_pre_post(intensity_col = "speed", pre_ex_intensity = 3) %>%
+    trim_pre_post(intensity_col = "speed", pre_ex_intensity = 5.6) %>%
+    select(time, speed, grade, vo2_rel, vo2_abs, vco2, ve, peto2, petco2) %>%
+    mutate(ve_vo2 = ve*1000 / vo2_abs,
+           ve_vco2 = ve*1000 / vco2)
+
+df_avg <- avg_exercise_test(df_unavg, type = "breath", subtype = "rolling",
+                            time_col = "time", roll_window = 9, roll_trim = 4)
+
+ss <- loop_v_slope(df_avg, "vo2_abs", "vco2")
+plot(ss)
+bp_idx <- v_slope(df_avg, "vo2_abs", "vco2")
+
+df_left <- df_avg[1:bp_idx,]
+df_right <- df_avg[(bp_idx+1):nrow(df_avg),]
+lm_left <- lm(vco2 ~ 1 + vo2_abs, data = df_left)
+lm_right <- lm(vco2 ~ 1 + vo2_abs, data = df_right)
+
+plot_data_left <- tibble(vo2_abs = df_left$vo2_abs,
+                         vco2 = predict(lm_left))
+plot_data_right <- tibble(vo2_abs = df_right$vo2_abs,
+                         vco2 = predict(lm_right))
+
+ggplot(data = df_avg, aes(x = vo2_abs, y = vco2)) +
+    geom_point(color = "purple", alpha = 0.5) +
+    geom_vline(xintercept = df_avg$vo2_abs[bp_idx]) +
+    geom_line(data = plot_data_left, aes(x = vo2_abs, y = vco2)) +
+    geom_line(data = plot_data_right, aes(x = vo2_abs, y = vco2)) +
+    geom_smooth(method = "lm", se = FALSE)
+
