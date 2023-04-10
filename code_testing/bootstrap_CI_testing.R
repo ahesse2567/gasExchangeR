d9f9c00615760a841fe6c088ad01dcf61c8943f8
ahesse2567 <ahesse2567@gmail.com>
2023-04-10 16:26:50 -0500

    Update bootstrap_CI_testing.R

--- code_testing/bootstrap_CI_testing.R
+++ code_testing/bootstrap_CI_testing.R
@@ -1,7 +1,7 @@
-x_threshold_boot <- function(data, i, .x, .y, df, degree) {
+x_threshold_boot <- function(.data, i, .x, .y, df, degree) {
 
     # find best number of knots
-    lm_spline <- loop_d2_reg_spline(.data = data[i,], .x = .x, .y = .y,
+    lm_spline <- loop_d2_reg_spline(.data = .data[i,], .x = .x, .y = .y,
                                     df = df, degree = degree)
 
     # It feels like there's a better way than splinefun to find a more exact
@@ -11,9 +11,9 @@ x_threshold_boot <- function(data, i, .x, .y, df, degree) {
     # a spline interpolation function. Use this with optimize() function.
 
     # get new values at equal spacing for a smoother splinefun result
-    equi_spaced_x <- seq(from = min(data[i,][[.x]]),
-                         to = max(data[i,][[.x]]),
-                         length.out = range(data[i,][[vo2]]) %>%
+    equi_spaced_x <- seq(from = min(.data[i,][[.x]]),
+                         to = max(.data[i,][[.x]]),
+                         length.out = range(.data[i,][[vo2]]) %>%
                              diff() %>%
                              round())
     pred <- stats::predict(lm_spline,
@@ -36,9 +36,11 @@ x_threshold_boot <- function(data, i, .x, .y, df, degree) {
                 x = equi_spaced_x[sign_change_idx],
                 deriv = 1) < 0)
             while(length(neg_slope_idx) > 0 & length(sign_change_idx) > 1) {
-                # remove the left-most value
-                neg_slope_idx <- neg_slope_idx[-1]
-                sign_change_idx <- sign_change_idx[neg_slope_idx]
+                # remove one value with a negative slope at a time in case
+                # all are negative. If the derivative is negative but it's
+                # right befor a clear upturn, that's probably the threshold
+                sign_change_idx <- sign_change_idx[-neg_slope_idx[1]]
+                # find which values have negative signs to exit loop
                 neg_slope_idx <- which(spline_func(
                     x = equi_spaced_x[sign_change_idx],
                     deriv = 1) < 0)
@@ -55,9 +57,14 @@ x_threshold_boot <- function(data, i, .x, .y, df, degree) {
                 x = equi_spaced_x[sign_change_idx],
                 deriv = 1) > 0)
             while(length(pos_slope_idx) > 0 & length(sign_change_idx) > 1) {
-                # remove the left-most value
-                pos_slope_idx <- pos_slope_idx[-1]
-                sign_change_idx <- sign_change_idx[pos_slope_idx]
+                # remove values with a positive slope by logical indexing
+                # should I still be removing these incrementally? in case the
+                # nadir isocapnic buffering period is VERY short-lived?
+                # I could see how the deriv could be negative, but the
+                # accel could be very positive. this would be okay if
+                # it were the only maxima (I think)
+                sign_change_idx <- sign_change_idx[-pos_slope_idx[1]]
+                # find values with positive slope again to exit loop
                 pos_slope_idx <- which(spline_func(
                     x = equi_spaced_x[sign_change_idx],
                     deriv = 1) > 0)
@@ -101,16 +108,56 @@ x_threshold_boot <- function(data, i, .x, .y, df, degree) {
         }
     }
 
-    dplyr::if_else(length(sign_change_idx) > 0,
-            equi_spaced_x[sign_change_idx],
-            NA)
+    if(length(sign_change_idx) > 0) {
+        return(equi_spaced_x[sign_change_idx])
+    } else {
+        return(NA)
+    }
+
 }
 copy_df <- .data
 
-boot::boot(data = copy_df,
+boot_res <- boot::boot(data = copy_df,
            statistic = x_threshold_boot,
-           R = 10,
+           R = 500,
+           sim = "ordinary",
            .x = .x,
            .y = .y,
            df = df,
-           degree = degree)
+           degree = degree,
+           parallel = "multicore")
+plot(boot_res)
+boot::boot.ci(boot_res, type = "perc")
+boot_ci <- boot::boot.ci(boot_res, type = "perc",
+                         h = log,
+                         hinv = exp)
+x_threshold
+
+se_boot <- sd(boot_res$t) / sqrt(length(boot_res$t))
+ci_95 <- x_threshold + c(-1, 1) * 1.96 * se_boot
+ci_95
+
+x <- c(0.2, 0.528, 0.11, 0.260, 0.091,
+       1.314, 1.52, 0.244, 1.981, 0.273,
+       0.461, 0.366, 1.407, 0.79, 2.266)
+
+
+x <- boot_res$t
+
+
+boxcox(model, data = x)
+
+quantile(boot_res$t, probs = c(2.5, 97.5)/100)
+
+library(MASS)
+b <- boxcox(lm(x ~ 1), data = data.frame(x = x))
+b
+
+boxcox(lm(boot_res$t ~ 1, data = data.frame(x = boot_res$t)))
+
+b <- boxcox(lm(x ~ 1, data = data.frame(x = boot_res$t)))
+b
+lambda <- b$x[which.max(b$y)]
+lambda
+
+hist((boot_res$t ^ lambda - 1) / lambda)
